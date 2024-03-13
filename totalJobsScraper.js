3c89f463fb6c5c1ed5dde3b2a860b8dbb38529f9
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2024-03-13 11:24:54 +0000

    incorporate basic error handling

--- totalJobsScraper.js
+++ totalJobsScraper.js
@@ -5,7 +5,10 @@ const writeCSV = require('./library/writeCSV');
 const writeMarkDown = require('./library/writeMarkDown');
 const formatDescription = require('./library/formatDescription');
 
-
+//  if error then....
+//  Note: error name... at ... point in process, error code?
+//  screenshot, save screenshot pathname to error log
+//  
 
 
 async function scrapeTotalJobs(url) {
@@ -18,16 +21,16 @@ async function scrapeTotalJobs(url) {
 
 
 
-    // await page.screenshot({path: 'totalJobsLoadScreenshot.png'})
+    await page.screenshot({path: 'totalJobsLoadScreenshot.png'})
 
     await page.waitForSelector('#ccmgt_explicit_preferences', { visible: true });
     await page.click('div.options-button'); // Primary selector for the close button
-    // await page.screenshot({path: 'totalJobsinterim.png'})
+    await page.screenshot({path: 'totalJobsinterim.png'})
     await page.waitForSelector('#ccmgt_preferences_reject', { visible: true });
     await page.click('#ccmgt_preferences_reject');
 
 
-    // await page.screenshot({path: 'totalJobsAfterPopupScreenshot.png'})
+    await page.screenshot({path: 'totalJobsAfterPopupScreenshot.png'})
 
 // //Close the popup/iframe
 // //  try {
@@ -74,13 +77,15 @@ await page.waitForSelector('div.res-130ff2y > div > h1 > span.res-vurnku', { vis
             const rawCompanyName = listing.querySelector('article.res-vxbpca > div > div > div.res-1r68twq')?.innerText;
             const rawLocation = listing.querySelector('article.res-vxbpca > div > div > div.res-qchjmw')?.innerText;
             const link = listing.querySelector('article.res-vxbpca > div > h2 > a')?.href;    
-       
+            const extractUrl = link.split('/')[5]
+            const id = extractUrl.split("-")[this.length].replace("job", '')
            //  remove commas
            const jobTitle = rawJobTitle.replaceAll(",", " ")
            const  companyName = rawCompanyName.replaceAll(",", " ")
            const  location = rawLocation.replaceAll(",", " ")
 
-       listings.push({ jobTitle, companyName, location, link });
+       listings.push({ id, jobTitle, companyName, location, link });
+       console.log({ id, jobTitle, companyName, location, link })
       
         });
  
@@ -88,24 +93,57 @@ await page.waitForSelector('div.res-130ff2y > div > h1 > span.res-vurnku', { vis
       });
       console.log("reading listings from page: ", i+1, " of ", resultsPages)
         allJobListings.push(paginatedListings)
-
-    // Go to next page
-      if(i != lastPage){
+        
        
-        await page.waitForSelector('a.res-1xvjmp2', { visible: true });
-        let nextLink = `https://www.totaljobs.com/jobs/work-from-home/react-remote?page=${i+2}&postedWithin=1`
+    // Navigate Pagination unless last page
+          if(i != lastPage){
 
-        // console.log("link to next page: ", nextLink )
-        await new Promise(resolve => setTimeout(resolve, 2000 + Math.random() * 5000)); 
-        await page.click('a.res-1xvjmp2');
-        await page.goto(nextLink, { waitUntil: 'networkidle0' });
-        await new Promise(resolve => setTimeout(resolve, 2000 + Math.random() * 5000));      
-        }
+            //----- Navigation -> imitate human pagination clicks ---
+              try {
+                await page.waitForSelector('div.results-container > div.job-results-row > div.stst-rlr > div > div > div > div > div > nav > ul > li', { visible: true });
+              } catch (error) {
+                await page.screenshot({path: 'totalJobsError.png'})
+                await page.waitForSelector('div.results-container > div.job-results-row > div.stst-rlr > div > div > div > div > div > nav', { visible: true });
+                await page.waitForSelector('div.results-container > div.job-results-row > div.stst-rlr > div > div > div > div > div > nav > ul', { visible: true });
+              }
             
-        }
-      
+            let nextLink = `https://www.totaljobs.com/jobs/work-from-home/react-remote?page=${i+2}&postedWithin=1`
 
- } else {
+            await new Promise(resolve => setTimeout(resolve, 2000 + Math.random() * 5000)); 
+            // await page.click('a.res-1xvjmp2');
+            let lastItem = await page.evaluate(() => { 
+            let list = document.querySelectorAll('div.results-container > div.job-results-row > div.stst-rlr > div > div > div > div > div > nav > ul > li').length
+            return list})
+            console.log(lastItem)
+            let nextPageSelector = 'div.results-container > div.job-results-row > div.stst-rlr > div > div > div > div > div > nav > ul > li:nth-child' + `(${lastItem})`
+            await page.click(nextPageSelector);
+            
+            
+            //----- Navigation -> redirect page ---
+            pageRedirect : for(let navTryCount = 0; navTryCount < 4;){
+                try {
+                  let success = await page.goto(nextLink, { waitUntil: 'networkidle0' });
+                  if (success){
+                    console.log("navigation to next page in pagination successful on attempt: ", navTryCount+1)
+                    break pageRedirect
+                  }
+                } catch (error) {
+                  console.log("error navigating to next page, from page: ", i+1, " to page ", i+2, " attempt no: ", navTryCount+1);
+                  await page.screenshot({path: 'tjErrPagNav.png'})
+                  await new Promise(resolve => setTimeout(resolve, 2000 + Math.random() * 5000));
+                  navTryCount++   
+                  continue  pageRedirect
+                }
+               
+              }
+            
+            await new Promise(resolve => setTimeout(resolve, 2000 + Math.random() * 5000));      
+          }  
+        }
+       } 
+       
+// ------------ If only one page of results --------------------
+       else {
 
     await autoScroll(page);
     const jobListings = await page.evaluate(() => {
@@ -116,21 +154,22 @@ await page.waitForSelector('div.res-130ff2y > div > h1 > span.res-vurnku', { vis
            const rawCompanyName = listing.querySelector('article.res-vxbpca > div > div > div.res-1r68twq')?.innerText;
            const rawLocation = listing.querySelector('article.res-vxbpca > div > div > div.res-qchjmw')?.innerText;
            const link = listing.querySelector('article.res-vxbpca > div > h2 > a')?.href;
-      
+           const extractUrl = link.split('/')[5]
+           const id = extractUrl.split("-")[this.length].replace("job", '')
 
           //  remove commas
           const jobTitle = rawJobTitle.replaceAll(",", " ")
           const  companyName = rawCompanyName.replaceAll(",", " ")
           const  location = rawLocation.replaceAll(",", " ")
 
-       listings.push({ jobTitle, companyName, location, link });
+       listings.push({id, jobTitle, companyName, location, link });
        
        });
        return listings;
      });
      allJobListings.push(jobListings)
-   
  }
+//  --------------------------------------------------------
 
 
         let flatJobListings = allJobListings.flat()
@@ -146,17 +185,23 @@ await page.waitForSelector('div.res-130ff2y > div > h1 > span.res-vurnku', { vis
 
     const newPage = await browser.newPage();
     
-    // Random delay to simulate human-like behavior
-    await new Promise(resolve => setTimeout(resolve, 2000 + Math.random() * 5000)); 
-    await newPage.goto(link, { waitUntil: 'networkidle0' });
-
-
-    // await page.screenshot({path: 'totalJobsAfterPopupScreenshot.png'})
-
-    await newPage.waitForSelector('body', { visible: true }); 
+    try {
+      await new Promise(resolve => setTimeout(resolve, 2000 + Math.random() * 5000)); 
+      await newPage.goto(link, { waitUntil: 'networkidle0' });
+    } catch (error) {
+      await newPage.screenshot({path: 'tjErrLinkNav.png'})
+      console.log("error going to job description page")
+    }
+   
+    try {
+      await newPage.waitForSelector('body', { visible: true });  
+    } catch (error) {
+      await newPage.screenshot({path: 'tjErrLinkSelector.png'})
+      console.log("error waiting for selector on job description page", linkCount)
+    }
+ 
     let jobDescription = await newPage.evaluate(() => {
         return document.querySelector('body')?.innerText
-      
     });
     
     
@@ -168,13 +213,13 @@ await page.waitForSelector('div.res-130ff2y > div > h1 > span.res-vurnku', { vis
     await newPage.close();
   }
 
-  
+  // await page.close();
   await browser.close();
   // writeCSV(filteredJobListings, "totalJobs")
-  writeMarkDown(filteredJobListings)
-  // return filteredJobListings
+  // writeMarkDown(filteredJobListings)
+  return filteredJobListings
  
 }
 
-scrapeTotalJobs('https://www.totaljobs.com/jobs/work-from-home/react-remote?postedWithin=1')
-// module.exports = scrapeTotalJobs
+// scrapeTotalJobs('https://www.totaljobs.com/jobs/work-from-home/react-remote?postedWithin=1')
+module.exports = scrapeTotalJobs
